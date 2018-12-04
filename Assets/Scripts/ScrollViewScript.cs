23681d74e3e8ca864a9cb86f2e0b7cc31f1d0b1a
Steven Golim <golim@ridi.co.jp>
2018-12-04 16:26:37 +0900

    12/04

---
+++ Assets/Scripts/ScrollViewScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollViewScript : MonoBehaviour {
+
+    public Button button;
+    int num_button = 10;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i< num_button; i++)
+        {
+            Button new_button = (Button)Instantiate(button);
+            new_button.transform.position = new Vector3(0, 150 - (i * 33), 0);
+            new_button.name = "ボタン " + (i+1);
+            new_button.GetComponentInChildren<Text>().text = "ボタン "+ (i+1) ; 
+            new_button.transform.SetParent(gameObject.transform, false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
